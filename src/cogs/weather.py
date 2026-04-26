c9ed935049ecc2dbbbdf46ce40466479ba3442dc
gelofi <gelogelofi@gmail.com>
2026-04-27 00:55:57 +0800

    Plotta v1.1

--- src/cogs/weather.py
+++ src/cogs/weather.py
@@ -16,12 +16,11 @@ async def weather(city: str):
             try:
                 weather_res = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=relative_humidity_2m,temperature_2m,precipitation,rain,wind_speed_10m,showers,apparent_temperature,weather_code,surface_pressure')
                 weather_data = weather_res.json()
-                if weather_res.status_code == 200:
-                    temperature = f"{weather_data['current']['temperature_2m']}{weather_data['current_units']['temperature_2m']}"
-                    humidity = f"{weather_data['current']['relative_humidity_2m']}{weather_data['current_units']['relative_humidity_2m']}"
-                    wind_speed = f"{weather_data['current']['wind_speed_10m']} {weather_data['current_units']['wind_speed_10m']}"
-                    surface_pressure = f"{weather_data['current']['surface_pressure']} {weather_data['current_units']['surface_pressure']}"
-                    return country, place, temperature, humidity, wind_speed, surface_pressure
+                temperature = f"{weather_data['current']['temperature_2m']}{weather_data['current_units']['temperature_2m']}"
+                humidity = f"{weather_data['current']['relative_humidity_2m']}{weather_data['current_units']['relative_humidity_2m']}"
+                wind_speed = f"{weather_data['current']['wind_speed_10m']} {weather_data['current_units']['wind_speed_10m']}"
+                surface_pressure = f"{weather_data['current']['surface_pressure']} {weather_data['current_units']['surface_pressure']}"
+                return country, place, temperature, humidity, wind_speed, surface_pressure
             except Exception as e:
                 print("An error occurred while fetching for weather information.", e)
                 return None, None, None, None, None, None
@@ -35,6 +34,7 @@ class Weather(commands.Cog):
 
     @app_commands.command(name='weather', description='Returns the current weather conditions of a target location.')
     async def weather(self, interaction: discord.Interaction, location: str):
+        await interaction.response.defer()
         country, place, temperature, humidity, wind_speed, surface_pressure = await weather(location)
         if country is not None:
             weather_embed = discord.Embed(
@@ -46,13 +46,13 @@ class Weather(commands.Cog):
             weather_embed.add_field(name='Wind speed', value=wind_speed, inline=False)
             weather_embed.add_field(name='Surface pressure', value=surface_pressure, inline=False)
             weather_embed.set_footer(text='Uses Open-Meteo API <3')
-            await interaction.response.send_message(embed=weather_embed)
+            await interaction.followup.send(embed=weather_embed)
         else :
             error_embed = discord.Embed(
                 title='Error',
                 description=f'An error occurred while fetching for the location.',
                 color=discord.Color.red()
             )
-            await interaction.response.send_message(embed=error_embed, ephemeral=True)
+            await interaction.followup.send(embed=error_embed, ephemeral=True)
 async def setup(bot):
     await bot.add_cog(Weather(bot))
