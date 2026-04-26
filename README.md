cdf53631542a37864806fbf8c9f075dbd513159d
gelofi <gelogelofi@gmail.com>
2026-04-26 20:17:19 +0800

    Revert "fist commit"
    This reverts commit 7240cd26f4839db9a0c83fac94538a1c3e456d2f.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Plotta
-A Discord bot created in Python to automate tasks with matplotlib and pandas.
--- src/client/command_handler.py
+++
@@ -1,9 +0,0 @@
-import os
-
-async def load(bot):
-    for filename in os.listdir('src/cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
-            print(f'Loaded {filename[:-3]} command')
-        else:
-            print(f'Skipped loading {filename}')
